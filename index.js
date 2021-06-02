a18ed93825ad7df9126ceed3d5716cb77b996fd8
Deepak Singh <singhdeepak5219@gmail.com>
2021-06-02 14:27:00 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ var numberofiterations = document.querySelectorAll(".drum").length;
 for(var i=0;i<numberofiterations;i++)
 
 {
-    document.querySelectorAll(".drum")[i].addEventListener("click", play_sound);
-
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var value = this.innerHTML;
+        
+        play_sound(value);
+        buttonAnimation(value);
+    });
+   
 }
+document.addEventListener("keydown", function(event){
+        
+    play_sound(event.key);
+    buttonAnimation(event.key);
 
-function play_sound()
+});
+
+function play_sound(key)
 {
-    var value = this.innerHTML;
-    switch (value) {
+   
+    switch (key) {
         case "w":
             var audio = new Audio('sounds/tom-1.mp3');
             audio.play();
@@ -56,47 +67,13 @@ function play_sound()
     }
 }
 
-document.addEventListener("keydown", (event) => {
 
-    switch (event.key) {
-        case "w":
-            var audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
-            
-            break;
-            case "a":
-                var audio = new Audio('sounds/tom-2.mp3');
-                audio.play();
-                
-                break;
-                case "s":
-                    var audio = new Audio('sounds/tom-3.mp3');
-                    audio.play();
-                    
-                    break;
-                    case "d":
-                        var audio = new Audio('sounds/tom-4.mp3');
-                        audio.play();
-                        
-                        break;
-                        case "j":
-                            var audio = new Audio('sounds/snare.mp3');
-                            audio.play();
-                            
-                            break;
-    
-                            case "k":
-                                var audio = new Audio('sounds/kick-bass.mp3');
-                                audio.play();
-                                
-                                break;
-    
-                                case "l":
-                                    var audio = new Audio('sounds/crash.mp3');
-                                    audio.play();
-                               
-        default:
-            break;
-    }
+function buttonAnimation( currentkey)
+{
+   var activebutton=  document.querySelector("." + currentkey);
+   activebutton.classList.add("pressed");
+   setTimeout(() => {
+       activebutton.classList.remove("pressed");
+   }, 100);
 
-});
+}
