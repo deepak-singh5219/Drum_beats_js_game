0ae557a7a56327e0bcde995d6292fb704bb71983
sanyam17-beep <sanyamsharma590128@gmail.com>
2021-06-28 11:49:25 +0530

    code reviewed

--- index.js
+++ index.js
@@ -22,44 +22,43 @@ document.addEventListener("keydown", function(event){
 
 function play_sound(key)
 {
-   
     switch (key) {
-        case "w":
+        case "a":
             var audio = new Audio('sounds/tom-1.mp3');
             audio.play();
             break;
-            case "a":
+            case "s":
                 var audio = new Audio('sounds/tom-2.mp3');
                 audio.play();
                  break;
-                case "s":
+                case "d":
                     var audio = new Audio('sounds/tom-3.mp3');
                     audio.play();
                     
                     break;
-                    case "d":
+                    case "f":
                         var audio = new Audio('sounds/tom-4.mp3');
                         audio.play();
                         
                         break;
-                        case "j":
+                        case "h":
                             var audio = new Audio('sounds/snare.mp3');
                             audio.play();
                             
                             break;
     
-                            case "k":
+                            case "j":
                                 var audio = new Audio('sounds/kick-bass.mp3');
                                 audio.play();
                                 
                                 break;
     
-                                case "l":
+                                case "k":
                                     var audio = new Audio('sounds/crash.mp3');
                                     audio.play();
                                     
                                     break;
-                                    case "p":
+                                    case "l":
                                     var audio = new Audio('sounds/kick-2.mp3');
                                     audio.play();
                                     
